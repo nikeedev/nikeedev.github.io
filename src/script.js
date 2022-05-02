2a233d4e302b3e0f9c5ca4d4ca4442957f8d2450
Nikita <69197950+nikeedev@users.noreply.github.com>
2022-05-02 15:53:26 +0200

    updates

--- src/script.js
+++ src/script.js
@@ -1,7 +1,21 @@
 var age = document.getElementById("age");
+var longprogrammingyears = document.getElementById("longprogrammingyears");
+var longprogrammingmonths = document.getElementById("longprogrammingmonths");
 
 var date = new Date();
 
 if (date.getDate() == 12 && date.getMonth() == 11) {
     age.innerHTML = date.getFullYear() - 2007 
+} else {
+    age.innerHTML = date.getFullYear() - 2007 - 1;
 }
+
+
+if (date.getFullYear() == 2020 && date.getMonth() == 8) {
+    longprogrammingyears.innerHTML = date.getFullYear() - 2020;
+    longprogrammingmonths.innerHTML = 0; 
+} else {
+    longprogrammingyears.innerHTML = date.getFullYear() - 2020 - 1;
+    longprogrammingmonths.innerHTML = 8 - date.getMonth() + 1;
+}
+
