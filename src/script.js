0e579732d063bcbc2aec3fb853913264c3339ee3
nikeedev <myftr12394@gmail.com>
2022-06-13 13:30:08 +0200

    updates

--- src/script.js
+++ src/script.js
@@ -1,7 +1,9 @@
+console.log("Made with " + "%c\u2764", "color:#fc4444; font-family: ; font-size: 25px;", "by nikeedev!")
+
+
 var age = document.getElementById("age");
 var longprogrammingyears = document.getElementById("longprogrammingyears");
 var longprogrammingmonths = document.getElementById("longprogrammingmonths");
-
 var date = new Date();
 
 if (date.getDate() == 12 && date.getMonth() == 11) {
