57a80fc8c8d73362fed4463a6005d7210c53897a
nikeedev <myftr12394@gmail.com>
2023-04-12 10:58:44 +0200

    updates

--- src/script.js
+++ src/script.js
@@ -2,12 +2,18 @@ console.log("Made with " + "%c\u2764", "color:#fc4444; font-family: ; font-size:
 
 
 var age = document.getElementById("age");
-var longprogrammingyears = document.getElementById("longprogrammingyears");
-var longprogrammingmonths = document.getElementById("longprogrammingmonths");
-var date = new Date();
 
-if (date.getDate() == 12 && date.getMonth() == 11) {
-    age.innerHTML = date.getFullYear() - 2007 
-} else {
-    age.innerHTML = date.getFullYear() - 2007 - 1;
+let my_birthday = new Date(2007, 11, 12);
+let dt2 = new Date();
+
+function diff_years(dt2, dt1) {
+
+    var diff = (dt2.getTime() - dt1.getTime()) / 1000;
+    diff /= (60 * 60 * 24);
+    return Math.abs(Math.round(diff / 365.25));
+
 }
+
+
+age.innerText = diff_years(my_birthday, dt2);
+
