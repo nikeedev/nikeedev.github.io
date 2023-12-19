a59d792aafb36b29d005dcbb272e66d225b14eb0
nikeedev <nikita.gon.dev@gmail.com>
2023-12-19 09:30:45 +0100

    up

--- script.js
+++ script.js
@@ -1,38 +1,20 @@
-let elem_projects = document.querySelector('.projects');
+const registerServiceWorker = async () => {
+    if ("serviceWorker" in navigator) {
+        try {
+            const registration = await navigator.serviceWorker.register("/sw.js", {
+                scope: "/",
+            });
+            if (registration.installing) {
+                console.log("Service worker installing");
+            } else if (registration.waiting) {
+                console.log("Service worker installed");
+            } else if (registration.active) {
+                console.log("Service worker active");
+            }
+        } catch (error) {
+            console.error(`Registration failed with ${error}`);
+        }
+    }
+};
 
-let data = await(await fetch("./projects.json")).json()
-
-function project(proj) {
-    let element = document.createElement('div');
-
-    element.id = "project";
-    
-    let header = document.createElement('h2');
-    header.innerText = proj.header;
-
-    let a = document.createElement('a');
-    a.href = proj.url;
-    a.target
-
-    let img = document.createElement('img');
-    img.src = proj.img;
-    img.style.imageRendering = "auto"
-    img.alt = proj.desc;
-    
-    a.appendChild(img);
-    let desc = document.createElement('p');
-
-    desc.innerText = proj.desc;
-
-    element.appendChild(header);
-    element.appendChild(a);
-    element.appendChild(document.createElement('br'))
-    element.appendChild(desc);
-    
-    return element;
-}
-
-for (let proj in data) {
-    console.log(data[proj]);
-    elem_projects.appendChild(project(data[proj]))
-}
+registerServiceWorker();
---
+++ sw.js
@@ -0,0 +1,54 @@
+const addResourcesToCache = async (resources) => {
+    const cache = await caches.open("v1");
+    await cache.addAll(resources);
+};
+
+self.addEventListener("install", (event) => {
+    event.waitUntil(
+        addResourcesToCache([
+            "/",
+            "/index.html",
+            "/style.css",
+            "/assets",
+            "/assets/github-mark-white.png",
+            "/assets/github-mark.png",
+            "/assets/nikee_256.png",
+            "/assets/nikee.png",
+            "/assets/opengl.png",
+            "/assets/Rain-Lang.png",
+            "/404.html",
+            "/manifest.json",
+            "/projects.html",
+            "/projects.js",
+            "/projects.json",
+            "/script.js",
+            "/welcome.txt"
+        ]),
+    );
+});
+
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(caches.match(event.request));
+});
+
+
+const putInCache = async (request, response) => {
+  const cache = await caches.open("v1");
+  await cache.put(request, response);
+};
+
+const cacheFirst = async (request) => {
+  const responseFromCache = await caches.match(request);
+  if (responseFromCache) {
+    return responseFromCache;
+  }
+  const responseFromNetwork = await fetch(request);
+  putInCache(request, responseFromNetwork.clone());
+  return responseFromNetwork;
+};
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(cacheFirst(event.request));
+});
+
