309e290c930a54ff99dca72e245ac1b55b104dae
nikeedev <nikita.gon.dev@gmail.com>
2023-12-19 11:41:05 +0100

    up

--- 404.js
+++ 404.js
@@ -1,3 +1,5 @@
+import router from "./router-dom.js";
+
 console.log("Current slash command: " + router.current_route);
 
 router.createRoute("github", "https://github.com/nikeedev");
--- router-dom.js
+++ router-dom.js
@@ -22,3 +22,5 @@ function update() {
     requestAnimationFrame(update);
 }
 requestAnimationFrame(update);
+
+export default router;
