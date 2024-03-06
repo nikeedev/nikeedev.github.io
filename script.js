6a79457ccfa5e6e5e597ca22e4496a5baf1e4fc8
nikeedev <nikita.gon.dev@gmail.com>
2024-03-06 09:07:45 +0100

    up

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
 const registerServiceWorker = async () => {
     if ("serviceWorker" in navigator) {
         try {
-            const registration = await navigator.serviceWorker.register("/sw.js", {
-                scope: "/",
-            });
+            const registration = await navigator.serviceWorker.register("/sw.js");
             if (registration.installing) {
                 console.log("Service worker installing");
             } else if (registration.waiting) {
