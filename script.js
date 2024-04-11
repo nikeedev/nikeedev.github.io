c83fba1a0b30a9cb6d653f532255171deda2fef6
nikeedev <nikita.gon.dev@gmail.com>
2024-04-11 18:02:23 +0200

    up

--- script.js
+++ script.js
@@ -15,6 +15,7 @@ const registerServiceWorker = async () => {
     }
 };
 
+// yep here is the hello command
 console.log("%cHi! I see you are looking at the code (or maybe you accidentally opened the console...) so you are up to something here...anyway, publish any fixes here you if you find any: https://github.com/nikeedev/!", "font-size: 20px; font-weight: bolder;")
 
-registerServiceWorker();
+// registerServiceWorker();
