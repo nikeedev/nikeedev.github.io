582c55d80716033e29f1fc771f0ca2e7531b6fbb
nikeedev <nikita.gon.dev@gmail.com>
2024-05-14 11:10:34 +0200

    up

--- script.js
+++ script.js
@@ -16,6 +16,6 @@ const registerServiceWorker = async () => {
 };
 
 // yep here is the hello command
-console.log("%cHi! I see you are looking at the code (or maybe you accidentally opened the console...) so you are up to something here...anyway, publish any fixes here you if you find any: https://github.com/nikeedev/nikeedev.github.io!", "font-size: 20px; font-weight: bolder;")
+console.log("%cHi! I see you are looking at the code (or maybe you accidentally opened the console...) so you are up to something here...anyway...publish any issues you if you find any on github (login required (free)): https://github.com/nikeedev/nikeedev.github.io!", "font-size: 20px; font-weight: 900;")
 
 // registerServiceWorker();
