61f76cc29ef9f8eab07f28f70344701de47073f2
nikeedev <nikita.gon.dev@gmail.com>
2024-08-20 20:09:05 +0200

    updates

--- script.js
+++ script.js
@@ -19,3 +19,26 @@ const registerServiceWorker = async () => {
 console.log("%cHi! I see you are looking at the code (or maybe you accidentally opened the console...) so you are up to something here...anyway...publish any issues you if you find any on github (login required (free)): https://github.com/nikeedev/nikeedev.github.io!", "font-size: 20px; font-weight: 900;")
 
 // registerServiceWorker();
+
+let theme = localStorage.getItem("theme");
+if (theme == null) {
+    theme = "Original";
+    localStorage.setItem("theme", theme);
+}
+
+let mainColor = "";
+let backColor = "";
+
+if (theme == "Original") {
+    mainColor = "#6271f7";
+    backColor = "#282828";
+} else if (theme == "blueorange") {
+    mainColor = "#1d2ec4";
+    backColor = "#ffbb46";
+} else if (theme == "pistachio") {  
+    mainColor = "#8affab";  
+    backColor = "#282828";
+}
+
+document.documentElement.style.setProperty("--main-color", mainColor);
+document.documentElement.style.setProperty("--back-color", backColor);
