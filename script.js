13846fe258621be44b4ab8c17d50f4ce50119ff3
nikeedev <nikita.gon.dev@gmail.com>
2025-04-09 13:53:29 +0200

    up

--- script.js
+++ script.js
@@ -15,6 +15,10 @@ const registerServiceWorker = async () => {
     }
 };
 
+if (navigator.userAgent.includes("Mobile")) {
+    document.querySelector("#wcb").style.display = "none";
+}
+
 // yep here is the hello command
 console.log("%cHi! I see you are looking at the code (or maybe you accidentally opened the console...) so you are up to something here...anyway...publish any issues you if you find any on github (login required (free)): https://github.com/nikeedev/nikeedev.github.io!", "font-size: 20px; font-weight: 900;")
 
